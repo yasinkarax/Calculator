f3a185b1f3af55c00edbffb86f3b286b0b51ef14
yasinkarax <yasinkara99@hotmail.com>
2023-06-25 18:23:08 +0300

    eslint installed

--- assets/main.js
+++ assets/main.js
@@ -1,76 +1,64 @@
 // variables
-let 
-math = document.querySelector('#math'),
-value = document.querySelector('#result-value'),
-buttons = document.querySelectorAll('button'),
-clear = document.querySelector('#clear'),
-del = document.querySelector('#del'),
-equal = document.querySelector('#equal'),
-buttonsL = buttons.length;
+const math = document.querySelector('#math');
+const value = document.querySelector('#result-value');
+const buttons = document.querySelectorAll('button');
+const clearIn = document.querySelector('#clear').innerHTML;
+const delIn = document.querySelector('#del').innerHTML;
+const equalIn = document.querySelector('#equal').innerHTML;
+const buttonsL = buttons.length;
+const operators = '+-*/.';
+const err = 'önce sayı girmelisiniz';
+const empty = '';
 
-
-const 
-operators = '+-*/.', 
-err1 = "önce sayı girmelisiniz", 
-empty = '';
-
-document.onload = calculator();
-
-function readyCal(){
-    math.innerHTML = empty;
-    value.innerHTML = empty;
+function readyCal() {
+  math.innerHTML = empty;
+  value.innerHTML = empty;
 }
 
-//calculate the result
-function calculate(){
-        return Intl.NumberFormat('tr-TR').format(Function("return " + math.innerHTML)())
+// calculate the result
+function calculate() {
+  return Intl.NumberFormat('tr-TR').format(Function('return ' + math.innerHTML)());
 }
 
 // returns the buttons's value
-let buttonInner = i => buttons[i].innerHTML;
+const buttonInner = (i) => buttons[i].innerHTML;
 
-
-function calculator(){
-    readyCal();
-    for(let i = 0; i < buttonsL; i++){
-        buttons[i].addEventListener('click', function (){
-            if (math.innerHTML == empty && operators.includes(buttonInner(i)))
-                math.innerHTML += err1;
-
-            else if (operators.includes(math.innerHTML[math.innerHTML.length - 1])  && operators.includes(buttonInner(i)))
-                math.innerHTML = math.innerHTML;
-
-            else if (buttonInner(i) == clear.innerHTML)
-                readyCal();
-
-
-            else if (buttonInner(i) == equal.innerHTML)
-                value.innerHTML = calculate();
-
-            else if (buttonInner(i) == del.innerHTML)
-                value.innerHTML = empty;
-                
-            else if(math.innerHTML == err1 && operators.includes(buttonInner(i)))
-                    math.innerHTML =  err1;
-            
-            else if(math.innerHTML == err1 && operators.includes(buttonInner(i)) == false)
-                math.innerHTML = buttonInner(i)
-
-            else if(value.innerHTML != empty){
-                    if(operators.includes(buttonInner(i))){
-                        math.innerHTML = value.innerHTML.replaceAll('.', '') + buttonInner(i); 
-                        value.innerHTML = empty;
-                    }
-                    else{
-                        math.innerHTML = buttonInner(i);
-                        value.innerHTML = empty;
-                    }
-            }
-            else{
-                math.innerHTML += buttonInner(i)
-                
-            }
-                
-        })
-    }
+function calculator() {
+  readyCal();
+  for (let i = 0; i < buttonsL; i += 1) {
+    buttons[i].addEventListener('click', () => {
+      if (math.innerHTML === empty && operators.includes(buttonInner(i))) {
+        math.innerHTML += err;
+      } else if (operators.includes(math.innerHTML[math.innerHTML.length - 1])
+          && operators.includes(buttonInner(i))) {
+        math.innerHTML = math.innerHTML;
+      } else if (buttonInner(i) === clearIn) {
+        readyCal();
+      } else if (buttonInner(i) === equalIn) {
+        value.innerHTML = calculate();
+      } else if (buttonInner(i) === delIn) {
+        if (value.innerHTML !== empty) {
+          value.innerHTML = empty;
+        } else {
+          math.innerHTML = math.innerHTML.slice(0, -1);
+        }
+      } else if (math.innerHTML === err && operators.includes(buttonInner(i))) {
+        math.innerHTML = err;
+      } else if (math.innerHTML === err && operators.includes(buttonInner(i)) === false) {
+        math.innerHTML = buttonInner(i);
+      } else if (value.innerHTML !== empty) {
+        if (operators.includes(buttonInner(i))) {
+          math.innerHTML = value.innerHTML.replaceAll('.', '') + buttonInner(i);
+          value.innerHTML = empty;
+        } else {
+          math.innerHTML = buttonInner(i);
+          value.innerHTML = empty;
+        }
+      } else {
+        math.innerHTML += buttonInner(i);
+      }
+    });
+  }
 }
+
+document.onload = calculator();
