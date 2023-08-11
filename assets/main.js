c14f914722b6bc6f07cb7705b4ce16b22b84a424
yasinkarax <yasinkara99@hotmail.com>
2023-08-12 02:03:31 +0300

    feat: the keyboard is available

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,19 @@
 const buttons = document.querySelectorAll('button');
 const buttonsL = buttons.length;
 const operators = ['+', '-', 'x', '÷', '.'];
-
+const result = document.querySelector('#result');
+const math = document.querySelector('#math');
 // listen all buttons
-for (let i = 0; i <= buttonsL; i += 1) {
+for (let i = 0; i <= buttonsL - 1; i += 1) {
   buttons[i].onclick = function cal(e) {
-    const result = document.querySelector('#result');
     const resultV = document.querySelector('#result').textContent;
-    const math = document.querySelector('#math');
-    let mathV = document.querySelector('#math').textContent;
     const buttonV = this.textContent;
+    let mathV = document.querySelector('#math').textContent;
     const del = this.id;
-
     if (buttonV === 'C') {
       math.textContent = '';
       result.textContent = '';
+      console.log(mathV);
     } else if (del === 'del') {
       if (resultV === '') {
         math.textContent = mathV.slice(0, resultV.length - 1);
@@ -47,3 +46,52 @@ for (let i = 0; i <= buttonsL; i += 1) {
     e.preventDefault();
   };
 }
+
+// listen to all keys
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('keydown', (event) => {
+    const kValue = event.key.replace(/[/]/, '÷').replace(/[*]/, 'x');
+    const resultV = document.querySelector('#result').textContent;
+    const mathV = document.querySelector('#math').textContent.replace(/[÷]/g, '/').replace(/[x]/g, '*');
+    const lastCharMath = math.textContent[math.textContent.length - 1];
+    const isKey = () => /[0-9+\-x÷.]/.test(kValue);
+    const isDigit = () => /[0-9]/.test(kValue);
+    const isOp = () => /[+\-÷x.]/.test(kValue);
+    console.log(kValue);
+    if (isKey()) {
+      if (math.textContent === '' && isOp() === true) {
+        math.textContent += '';
+      } else if (isOp()) {
+        if (/[+\-÷x.]/.test(lastCharMath)) {
+          math.textContent += '';
+        } else if (resultV !== '') {
+          if (/./.test(resultV) && kValue === '.') {
+            result.textContent += '';
+          } else {
+            math.textContent = result.textContent.slice(2) + kValue;
+            result.textContent = '';
+          }
+        } else {
+          math.textContent += kValue;
+        }
+      } else if (isDigit() && resultV !== '') {
+        math.textContent = kValue;
+        result.textContent = '';
+      } else {
+        math.textContent += kValue;
+      }
+    } else if (kValue === 'Backspace') {
+      if (resultV !== '') {
+        result.textContent = '';
+      } else {
+        math.textContent = mathV.slice(0, resultV.length - 1);
+      }
+    } else if (kValue === 'Enter') {
+      if (/[+\-/*.]/.test(mathV)) {
+        result.textContent = '= ' + eval(mathV);
+      } else {
+        math.textContent += '';
+      }
+    }
+  });
+});
